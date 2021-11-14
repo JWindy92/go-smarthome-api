1798f6aae8c51d28c438ae28911b642235860f22
John Widmaier <jwidmaier@officepracticum.com>
2021-11-14 08:23:33 -0500

    removed commented code

--- main.go
+++ main.go
@@ -10,12 +10,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// type Device struct {
-// 	Id    string `json:"Id"`
-// 	Name  string `json:"Name"`
-// 	State string `json:"State"`
-// }
-
 func allDeviceHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("[GET] /devices")
 	var result = getAllDevices()
@@ -48,47 +42,6 @@ func deleteDeviceHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("[DELETE] /devices/%s", id)
 	deleteDevice(id)
 }
-
-// func getAllDevices(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Println("[GET] /devices")
-// 	json.NewEncoder(w).Encode(DummyDB)
-// }
-
-// func getDeviceById(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	key := vars["id"]
-
-// 	fmt.Printf("[GET] /devices/%s", key)
-
-// 	for _, device := range DummyDB {
-// 		if device.Id == key {
-// 			json.NewEncoder(w).Encode(device)
-// 		}
-// 	}
-// }
-
-// func createNewDevice(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Println("[POST] /devices")
-// 	reqBody, _ := ioutil.ReadAll(r.Body)
-// 	var device Device
-// 	json.Unmarshal(reqBody, &device) // What is Unmarshal? What is '&' doing?
-
-// 	DummyDB = append(DummyDB, device)
-
-// 	json.NewEncoder(w).Encode(device)
-// }
-
-// func deleteDevice(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	id := vars["id"]
-
-// 	fmt.Printf("[DELETE] /devices/%s", id)
-// 	for idx, device := range DummyDB {
-// 		if device.Id == id {
-// 			DummyDB = append(DummyDB[:idx], DummyDB[idx+1:]...)
-// 		}
-// 	}
-// }
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true) // What is StrictSlash?
