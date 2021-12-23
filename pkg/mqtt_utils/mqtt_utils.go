330d500777a3887ab725dac97d3d9f5de1323471
John Widmaier <jwidmaier@officepracticum.com>
2021-12-23 17:16:02 -0500

    added todo

--- pkg/mqtt_utils/mqtt_utils.go
+++ pkg/mqtt_utils/mqtt_utils.go
@@ -7,8 +7,9 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+// TODO: These should be read from environment variables
 var broker = "10.0.0.228"
-var port = 9002
+var port = 9001
 
 var Zap = zap.NewLogger()
 
